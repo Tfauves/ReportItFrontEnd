b7d7e35c7071eae4d3226302d7a380a93d45d8c9
Tranquility Base <tfauvelle@gmail.com>
2022-12-12 14:46:40 -0500

    corrected link from admin view button

--- src/components/AreaAdmin/AdminView.js
+++ src/components/AreaAdmin/AdminView.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Unstable_Grid2";
 
 const AdminView = () => {
   const navigate = useNavigate();
@@ -16,13 +12,6 @@ const AdminView = () => {
   const handleNavigateAbout = () => {
     navigate("/about");
   };
-
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  }));
 
   return (
     <div>
@@ -58,7 +47,7 @@ const AdminView = () => {
               style={{
                 backgroundColor: "gray",
               }}
-              onClick={handleNavigateAbout}
+              onClick={handleNavigate}
               variant="contained"
               size="large"
             >
