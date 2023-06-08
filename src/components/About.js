fcba9ca188b6c7606e03106c6b4fb804d5774015
Travis J <tfauvelle@gmail.com>
2023-06-08 16:28:10 -0400

    style work for responsive design

--- src/components/About.js
+++ src/components/About.js
@@ -1,103 +1,53 @@
 import React from "react";
 import sideImg from "../assets/reportItAbout.jpg";
 import Splash from "./common/Splash";
+import "../../src/About.css";
 
 const About = () => {
   return (
-    <div>
-      <div
-        className="row"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          width: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div
-          className="column"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            flexBasis: "100%",
-            flex: "1",
-            backgroundColor: "#d5d3db",
-            height: "100vh",
-          }}
-        >
-          <h1
-            style={{
-              textDecoration: "underline",
-              color: "#303030",
-              marginLeft: ".5em",
-              fontFamily: "Lato, sans-serif",
-            }}
-          >
-            About Us
-          </h1>
-          <p style={{ padding: "1em", color: "#303030" }}>
+    <div className="about-container">
+      <div className="text-column">
+        <h1 className="about-heading">About Us</h1>
+        <div className="about-content">
+          <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
             mollitia, molestiae quas vel sint commodi repudiandae consequuntur
             voluptatum laborum numquam blanditiis harum quisquam eius sed odit
             fugiat iusto fuga praesentium optio, eaque rerum!
           </p>
-          <div style={{}}>
-            <p style={{ padding: "1em", color: "#303030" }}>
-              officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-              nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo
-              neque error repudiandae fuga? Ipsa laudantium molestias eos
-              sapiente officiis modi at sunt excepturi expedita sint? Sed
-              quibusdam recusandae alias error harum maxime adipisci amet
-              laborum.
-            </p>
-          </div>
-          <div style={{}}>
-            <p style={{ padding: "1em", color: "#303030" }}>
-              nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo
-              neque error repudiandae fuga? Ipsa laudantium molestias eos
-              sapiente officiis modi at sunt excepturi expedita sint? Sed
-              quibusdam recusandae alias error harum maxime adipisci amet
-              laborum. Perspiciatis minima nesciunt dolorem! Officiis iure rerum
-              voluptates a cumque velit
-            </p>
-          </div>
-          <div style={{}}>
-            <p style={{ padding: "1em", color: "#303030" }}>
-              Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
-              laudantium molestias eos sapiente officiis modi at sunt excepturi
-              expedita sint? Sed quibusdam recusandae alias error harum maxime
-              adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
-              Officiis iure rerum voluptates a cumque velit
-            </p>
-          </div>
-          <div style={{}}>
-            <p style={{ padding: "1em", color: "#303030" }}>
-              Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
-              laudantium molestias eos sapiente officiis modi at sunt excepturi
-              expedita sint? Sed quibusdam recusandae alias error harum maxime
-              adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
-              Officiis iure rerum voluptates a cumque velit
-            </p>
-          </div>
+          <p>
+            Officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+            nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+            error repudiandae fuga? Ipsa laudantium molestias eos sapiente
+            officiis modi at sunt excepturi expedita sint? Sed quibusdam
+            recusandae alias error harum maxime adipisci amet laborum.
+          </p>
+          <p>
+            Nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+            error repudiandae fuga? Ipsa laudantium molestias eos sapiente
+            officiis modi at sunt excepturi expedita sint? Sed quibusdam
+            recusandae alias error harum maxime adipisci amet laborum.
+            Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates
+            a cumque velit
+          </p>
+          <p>
+            Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
+            laudantium molestias eos sapiente officiis modi at sunt excepturi
+            expedita sint? Sed quibusdam recusandae alias error harum maxime
+            adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+            Officiis iure rerum voluptates a cumque velit
+          </p>
+          <p>
+            Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
+            laudantium molestias eos sapiente officiis modi at sunt excepturi
+            expedita sint? Sed quibusdam recusandae alias error harum maxime
+            adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+            Officiis iure rerum voluptates a cumque velit
+          </p>
         </div>
-
-        <div
-          className="column"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            flexBasis: "100%",
-            flex: "1",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "black",
-            height: "100vh",
-          }}
-        >
-          <Splash style={{ height: "100vh" }} image={sideImg} />
-        </div>
+      </div>
+      <div className="image-column">
+        <Splash className="image-splash" image={sideImg} />
       </div>
     </div>
   );
--- src/components/Home/Landing.js
+++ src/components/Home/Landing.js
@@ -16,56 +16,20 @@ const Landing = () => {
 
   const displayLanding = () => {
     return (
-      <div style={{ backgroundColor: "#1c1c1b" }}>
+      <div style={styles.container}>
         <Container>
-          <h1
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "2em",
-              marginBottom: "1.5em",
-              color: "#f1f1f1",
-              letterSpacing: "7px",
-              fontSize: "3em",
-              fontFamily: "Lato, sans-serif",
-            }}
-          >
-            Want To Be A Civic Crusader?
-          </h1>
-          <h4
-            style={{
-              color: "#a2a2a6",
-              letterSpacing: "3px",
-              marginTop: "-3em",
-              marginBottom: "-.5em",
-              fontFamily: "Lato, sans-serif",
-              fontWeight: "400",
-              letterSpacing: "3px",
-            }}
-          >
-            report issues in your area
-          </h4>
-          <Splash
-            image={landingImg}
-            style={{
-              marginTop: "2em",
-              height: "55vh",
-            }}
-          />
+          <h1 style={styles.title}>Want To Be A Civic Crusader?</h1>
+          <h4 style={styles.subtitle}>Report Issues in Your Area</h4>
+          <Splash image={landingImg} style={styles.splash} />
 
           <Button
-            style={{
-              marginTop: "2em",
-              width: "18%",
-              height: "5vh",
-              letterSpacing: "3px",
-            }}
+            style={styles.button}
             size="large"
             variant="contained"
             color="secondary"
             onClick={onClick}
           >
-            Signup
+            Sign up
           </Button>
         </Container>
       </div>
@@ -73,6 +37,85 @@ const Landing = () => {
   };
 
   return displayLanding();
+};
+
+const styles = {
+  container: {
+    backgroundColor: "#1c1c1b",
+  },
+  title: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "2em",
+    marginBottom: "1.5em",
+    color: "#f1f1f1",
+    letterSpacing: "2px",
+    fontSize: "3em",
+    fontFamily: "Lato, sans-serif",
+  },
+  subtitle: {
+    color: "#a2a2a6",
+    marginTop: "-1em",
+    marginBottom: "-.5em",
+    fontFamily: "Lato, sans-serif",
+    fontWeight: "400",
+    letterSpacing: "2px",
+  },
+  splash: {
+    marginTop: "2em",
+    height: "55vh",
+  },
+  button: {
+    marginTop: "2em",
+    width: "18%",
+    height: "5vh",
+    letterSpacing: "2px",
+  },
+};
+
+// Media Queries
+const mediaQueries = {
+  laptop: "@media (max-width: 1024px)",
+  tablet: "@media (max-width: 768px)",
+  mobile: "@media (max-width: 480px)",
+};
+
+// Responsive Styles
+styles.title[mediaQueries.tablet] = {
+  fontSize: "2.5em",
+};
+
+styles.subtitle[mediaQueries.tablet] = {
+  marginTop: "-0.5em",
+  marginBottom: "-0.5em",
+};
+
+styles.splash[mediaQueries.tablet] = {
+  marginTop: "1.5em",
+  height: "40vh",
+};
+
+styles.button[mediaQueries.tablet] = {
+  width: "30%",
+};
+
+styles.title[mediaQueries.mobile] = {
+  fontSize: "1.8em",
+  marginBottom: "0.5em",
+};
+
+styles.subtitle[mediaQueries.mobile] = {
+  marginTop: "-0.2em",
+  marginBottom: "-0.2em",
+};
+
+styles.splash[mediaQueries.mobile] = {
+  marginTop: "1em",
+  height: "30vh",
+};
+
+styles.button[mediaQueries.mobile] = {
+  width: "50%",
 };
 
 export default Landing;
