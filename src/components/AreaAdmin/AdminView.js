882ccb663b9619c10791ed2c5a5ce5e52c947d4b
Travis J <tfauvelle@gmail.com>
2023-10-11 15:20:14 -0400

    trying to track a login bug

--- src/components/AreaAdmin/AdminView.js
+++ src/components/AreaAdmin/AdminView.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
 import CustomBtn from "../common/CustomBtn";
 
 const AdminView = () => {
